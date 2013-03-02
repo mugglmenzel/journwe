b653f59fcda4f0bc4c7456d25c65160b6356f8de
mugglmenzel <mugglmenzel@gmail.com>
2013-03-02 08:20:29 +0100

    category edit (not complete yet)

--- code/journwe-webapp/app/controllers/CategoryController.java
+++ code/journwe-webapp/app/controllers/CategoryController.java
@@ -5,7 +5,7 @@ import models.Category;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.category.create;
+import views.html.category.manage;
 import controllers.dao.CategoryDAO;
 
 public class CategoryController extends Controller {
@@ -13,22 +13,28 @@ public class CategoryController extends Controller {
 	private static Form<Category> catForm = form(Category.class);
 
 	public static Result create() {
-		return ok(create.render(null, catForm, new CategoryDAO().all(50)));
+		return ok(manage.render(null, catForm, new CategoryDAO().all(50)));
+	}
+
+	public static Result edit(String id) {
+		Form<Category> editCatForm = catForm.fill(new CategoryDAO().get(id));
+		return ok(manage.render(null, editCatForm, new CategoryDAO().all(50)));
 	}
 
 	public static Result save() {
 		Form<Category> filledCatForm = catForm.bindFromRequest();
 		if (filledCatForm.hasErrors()) {
-			return badRequest(create.render("Please fill out the form correctly.", filledCatForm,
+			return badRequest(manage.render(
+					"Please fill out the form correctly.", filledCatForm,
 					new CategoryDAO().all(50)));
 		} else {
 			Category cat = filledCatForm.get();
 			if (new CategoryDAO().save(cat))
-				return ok("Wow, Congratulations!");
+				return ok(manage.render("Wow, Congratulations!", catForm, new CategoryDAO().all(50)));
 			else
-				return internalServerError(create.render("Something went wrong during saving :(", filledCatForm,
+				return internalServerError(manage.render(
+						"Something went wrong during saving :(", filledCatForm,
 						new CategoryDAO().all(50)));
 		}
 	}
-
 }
