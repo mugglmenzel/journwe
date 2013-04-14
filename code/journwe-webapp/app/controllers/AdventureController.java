ef38083c7669c3a4a5de279cab558f2afb3e36e7
mugglmenzel <mugglmenzel@gmail.com>
2013-04-14 22:37:33 +0200

    adventure controller save

--- code/journwe-webapp/app/controllers/AdventureController.java
+++ code/journwe-webapp/app/controllers/AdventureController.java
@@ -1,12 +1,7 @@
 package controllers;
 
-import static play.data.Form.form;
-
-import java.util.Map;
-
 import controllers.dao.AdventureDAO;
 import controllers.dao.InspirationDAO;
-
 import models.Adventure;
 import play.Logger;
 import play.data.Form;
@@ -15,31 +10,39 @@ import play.mvc.Result;
 import views.html.adventure.create;
 import views.html.adventure.get;
 
+import java.util.Map;
+
+import static play.data.Form.form;
+
 public class AdventureController extends Controller {
 
-	private static Form<Adventure> advForm = form(Adventure.class);
+    private static Form<Adventure> advForm = form(Adventure.class);
 
-	public static Result get(String catId, String insId, String id) {
-		return ok(get.render(new AdventureDAO().get(insId, id), new InspirationDAO().get(catId, insId)));
-	}
-	
-	public static Result create() {
-		Logger.info("Test");
-		Map<String, String> inspireOptions = new InspirationDAO()
-				.allOptionsMap(50);
-		Logger.info("Created Options List");
-		return ok(create.render(advForm, inspireOptions));
+    public static Result get(String catId, String insId, String id) {
+        return ok(get.render(new AdventureDAO().get(insId, id), new InspirationDAO().get(catId, insId)));
+    }
 
-	}
+    public static Result create() {
+        Logger.info("Test");
+        Map<String, String> inspireOptions = new InspirationDAO()
+                .allOptionsMap(50);
+        Logger.info("Created Options List");
+        return ok(create.render(advForm, inspireOptions));
 
-	public static Result save() {
-		Form<Adventure> filledAdvForm = advForm.bindFromRequest();
-		if (filledAdvForm.hasErrors()) {
-			return badRequest(create.render(filledAdvForm,
-					new InspirationDAO().allOptionsMap(50)));
+    }
 
-		} else
-			return ok("Wow, Congratulations!");
-	}
+    public static Result save() {
+        Form<Adventure> filledAdvForm = advForm.bindFromRequest();
+        if (filledAdvForm.hasErrors()) {
+            return badRequest(create.render(filledAdvForm,
+                    new InspirationDAO().allOptionsMap(50)));
+
+        } else {
+            Adventure adv = filledAdvForm.get();
+            new AdventureDAO().save(adv);
+            return ok(get.render(new AdventureDAO().get(adv.getInspirationId(), adv.getId()), new InspirationDAO().get(null, adv.getInspirationId())));
+        }
+
+    }
 
 }
