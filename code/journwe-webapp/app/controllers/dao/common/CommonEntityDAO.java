95671874a4bc40c5340017f3f75f3efbe44e568e
mugglmenzel <mugglmenzel@gmail.com>
2013-04-15 12:12:40 +0200

    java type variable fix

--- code/journwe-webapp/app/controllers/dao/common/CommonEntityDAO.java
+++ code/journwe-webapp/app/controllers/dao/common/CommonEntityDAO.java
@@ -5,7 +5,7 @@ import java.lang.reflect.ParameterizedType;
 public abstract class CommonEntityDAO<T> extends CommonDAO<T> implements IParentDAO<T> {
 
     public T get(String id) {
-        return pm.load((Class<T>) (((ParameterizedType) CommonEntityDAO.class.getGenericSuperclass()).getActualTypeArguments()[0]), id);
+        return pm.load((Class<T>) (((ParameterizedType) CommonEntityDAO.class.getGenericSuperclass()).getActualTypeArguments()[0]).getClass(), id);
     }
 
     public boolean delete(String id) {
