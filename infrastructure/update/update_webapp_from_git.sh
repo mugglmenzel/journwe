60e8c8a41fe0a99131cd113857ef499d444c6030
mugglmenzel <mugglmenzel@gmail.com>
2013-05-26 20:01:57 +0200

    update script with AWS API tools, update for all instances of load balancer (via param)

--- infrastructure/update/update_webapp_from_git.sh
+++ infrastructure/update/update_webapp_from_git.sh
@@ -1,41 +1,70 @@
 #!/bin/sh
 
+[ $# -lt 1 ] && { echo "Usage: $0 <Load Balancer Name>"; exit 1; }
+
+
 #cd $(dirname $0)
 current_path=$(pwd)
 
-ssh_opts_background=`echo -f -i $current_path/journwe.pem`
-ssh_opts_terminal=`echo -t -i $current_path/journwe.pem`
+ssh_opts_background=`echo -f -q -i $current_path/journwe.pem`
+ssh_opts_terminal=`echo -t -q -i $current_path/journwe.pem`
 scp_opts=`echo -i $current_path/journwe.pem`
-ssh_host="ubuntu@$1"
+
 #scp $scp_opts ./update_webapp_remote_from_git.sh $ssh_host:/home/ec2-user/ 
 # ssh $ssh_opts_terminal $ssh_host "sudo su"
 #ssh $ssh_opts_terminal $ssh_host "sudo sh /home/ec2-user/update_webapp_remote_from_git.sh"
-cd $current_path/../../code/journwe-webapp/
 
 echo "-----------------------------------------------------"
 echo "--- JournWe Update Script for Ubuntu EC2 machines ---"
 echo "-----------------------------------------------------"
 echo ""
-echo "--- infiltrating EC2 machine $1"
-echo "preparing machine..."
-ssh $ssh_opts_terminal $ssh_host "sudo aptitude -y update"
-ssh $ssh_opts_terminal $ssh_host "sudo aptitude -y install unzip"
-ssh $ssh_opts_terminal $ssh_host "sudo aptitude -y install openjdk-6-jre"
-
-echo "--- compiling JournWe web app"
+echo "-- compiling JournWe web app"
 echo "compiling staged play web app..."
+cd $current_path/../../code/journwe-webapp/
 play -Dconfig.file=conf/application-prod.conf clean compile dist
-echo "copying play web app distribution to ec2 host..."
-scp $scp_opts dist/journwe-*.zip $ssh_host:/home/ubuntu/ 
 
-echo "--- installing JournWe web app"
-echo "unzipping..."
-ssh $ssh_opts_terminal $ssh_host "unzip -q -o journwe-*.zip -d journwe"
-echo "chmoding..."
-ssh $ssh_opts_terminal $ssh_host "sudo chmod -R 777 journwe"
-echo "killing running java software..."
-ssh $ssh_opts_terminal $ssh_host "sudo killall java || true"
-echo "starting play web server..."
-ssh $ssh_opts_terminal $ssh_host "sudo journwe/journwe-*/start -Dhttp.port=80 >out 2>1 &"
+echo "-- preparing EC2 access"
+export AWS_ELB_HOME=$current_path/../elastic-load-balancing
+export EC2_HOME=$current_path/../ec2-api-tools
+export PATH=$PATH:$AWS_ELB_HOME/bin:$EC2_HOME/bin
+export AWS_CREDENTIAL_FILE=$AWS_ELB_HOME/credentials.properties
+export AWS_ACCESS_KEY=AKIAIPB6HBIM6TMNFRXA 
+export AWS_SECRET_KEY=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  
 
-echo "--- update complete."
+echo "-- checking for available servers in load balancer $1 ..."
+for instanceid in `elb-describe-instance-health $1 | awk '{print $2}'`; do
+	ipaddress=`ec2-describe-instances $instanceid | grep INSTANCE | awk '{print $4}'`
+	ssh_host="ubuntu@$ipaddress"
+	
+	echo "---- infiltrating EC2 machine $ipaddress"
+	echo "----- preparing software on machine..."
+	ssh $ssh_opts_terminal $ssh_host "sudo aptitude -yq update"
+	ssh $ssh_opts_terminal $ssh_host "sudo aptitude -yq install unzip"
+	ssh $ssh_opts_terminal $ssh_host "sudo aptitude -yq install openjdk-6-jre"
+#	echo "--- setting user rights"
+#	ssh $ssh_opts_terminal $ssh_host "sudo addgroup www-data"
+#	ssh $ssh_opts_terminal $ssh_host "sudo adduser www-data --ingroup www-data"
+	echo "---- copying startup script to $ipaddress..."
+	scp $scp_opts $current_path/journwe.conf $ssh_host:/home/ubuntu/
+	ssh $ssh_opts_terminal $ssh_host "sudo mv journwe.conf /etc/init/"
+	ssh $ssh_opts_terminal $ssh_host "sudo initctl reload-configuration"
+
+
+
+	echo "---- installing JournWe web app on $ipaddress"
+	echo "----- copying play web app distribution to $ipaddress..."
+	scp $scp_opts dist/journwe-*.zip $ssh_host:/home/ubuntu/ 
+	echo "----- unzipping..."
+	ssh $ssh_opts_terminal $ssh_host "unzip -q -o journwe-*.zip -d journwe"
+	echo "----- chmoding..."
+	ssh $ssh_opts_terminal $ssh_host "sudo chmod -R 777 journwe"
+	echo "----- stopping play web server..."
+	ssh $ssh_opts_terminal $ssh_host "sudo initctl stop journwe"
+	echo "----- starting play web server..."
+	#ssh $ssh_opts_terminal $ssh_host "sudo sh journwe/journwe-*/start -Dhttp.port=80 >out 2>&1 &"
+	ssh $ssh_opts_terminal $ssh_host "sudo initctl start journwe"
+	
+	echo "---- finished updating $ipaddress"
+done;
+
+echo "-- update complete."
