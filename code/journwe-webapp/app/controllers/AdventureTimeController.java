1456f2244bc0fa4d069a04c5267e3a702aee7f2f
mugglmenzel <mugglmenzel@gmail.com>
2013-07-08 01:09:55 +0200

    enabled time option voting, TODO: ajaxify add time option form

--- code/journwe-webapp/app/controllers/AdventureTimeController.java
+++ code/journwe-webapp/app/controllers/AdventureTimeController.java
@@ -5,16 +5,25 @@ import controllers.auth.SecuredAdminUser;
 import models.Inspiration;
 import models.adventure.Adventure;
 import models.adventure.Adventurer;
+import models.adventure.EPreferenceVote;
+import models.adventure.place.PlaceAdventurerPreference;
+import models.adventure.place.PlaceOption;
+import models.adventure.time.TimeAdventurerPreference;
 import models.adventure.time.TimeOption;
 import models.dao.*;
 import models.user.User;
+import org.codehaus.jackson.node.ObjectNode;
+import play.Logger;
 import play.data.Form;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Security;
-import views.html.adventure.getTime;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import static play.data.Form.form;
 
@@ -31,9 +40,25 @@ public class AdventureTimeController extends Controller {
 
     @Security.Authenticated(SecuredAdminUser.class)
     public static Result getTimes(String advId) {
-        return ok(Json.toJson(new TimeOptionDAO().all(advId)));
+        User usr = new UserDAO().findByAuthUserIdentity(PlayAuthenticate.getUser(Http.Context.current()));
+
+        List<ObjectNode> times = new ArrayList<ObjectNode>();
+        for (TimeOption to : new TimeOptionDAO().all(advId)) {
+            ObjectNode node = Json.newObject();
+            node.put("id", to.getId());
+            node.put("startDate", to.getStartDate() != null  ? new SimpleDateFormat().format(to.getStartDate()) : "");
+            node.put("endDate", to.getEndDate() != null ? new SimpleDateFormat().format(to.getEndDate()): "");
+            TimeAdventurerPreference pref = new TimeAdventurerPreferenceDAO().get(to.getId(), usr.getId());
+            node.put("vote", (pref != null) ? pref.getVote().toString() : EPreferenceVote.MAYBE.toString());
+            node.put("voteCount", Json.toJson(new TimeAdventurerPreferenceDAO().counts(to.getId())));
+
+            times.add(node);
+        }
+
+        return ok(Json.toJson(times));
     }
 
+    /*
     public static Result getTime(String advId) {
         Adventure adv = new AdventureDAO().get(advId);
         Inspiration ins = new InspirationDAO().get(adv.getInspirationId());
@@ -41,12 +66,36 @@ public class AdventureTimeController extends Controller {
         Adventurer advr = new AdventurerDAO().get(advId, usr.getId());
         return ok(getTime.render(adv, ins, advr, timeForm));
     }
+    */
 
-    public static Result saveTime(String advId) {
+    public static Result addTime(String advId) {
         TimeOption opt = form(TimeOption.class).bindFromRequest().get();
         opt.setAdventureId(advId);
         new TimeOptionDAO().save(opt);
-        return getTime(advId);
+        return ok(Json.toJson(opt));
+    }
+
+    public static Result vote(String optId) {
+        User usr = new UserDAO().findByAuthUserIdentity(PlayAuthenticate.getUser(Http.Context.current()));
+
+        String vote = form().bindFromRequest().get("vote").toUpperCase();
+
+        TimeAdventurerPreference pref = new TimeAdventurerPreferenceDAO().get(optId, usr.getId());
+        if (pref == null) {
+            pref = new TimeAdventurerPreference();
+            pref.setTimeOptionId(optId);
+            pref.setAdventurerId(usr.getId());
+        }
+
+        try {
+            pref.setVote(EPreferenceVote.valueOf(vote));
+        } catch (IllegalArgumentException e) {
+            Logger.error("Got unknown value for vote! value: " + vote);
+        }
+
+        new TimeAdventurerPreferenceDAO().save(pref);
+
+        return ok(Json.toJson(pref));
     }
 
 
--- code/journwe-webapp/app/models/dao/TimeAdventurerPreferenceDAO.java
+++ code/journwe-webapp/app/models/dao/TimeAdventurerPreferenceDAO.java
@@ -1,7 +1,17 @@
 package models.dao;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+import models.adventure.EPreferenceVote;
+import models.adventure.place.PlaceAdventurerPreference;
 import models.adventure.time.TimeAdventurerPreference;
 import models.dao.common.CommonRangeEntityDAO;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,5 +24,23 @@ public class TimeAdventurerPreferenceDAO extends CommonRangeEntityDAO<TimeAdvent
 
     public TimeAdventurerPreferenceDAO() {
         super(TimeAdventurerPreference.class);
+    }
+
+    public List<TimeAdventurerPreference> all(String placeOptionId) {
+        DynamoDBScanExpression scan = new DynamoDBScanExpression();
+        scan.addFilterCondition("timeoptionid", new Condition().withComparisonOperator(ComparisonOperator.EQ).withAttributeValueList(new AttributeValue(placeOptionId)));
+        return pm.scan(TimeAdventurerPreference.class, scan);
+    }
+
+    public Map<EPreferenceVote, Long> counts(String placeOptionId) {
+        Map<EPreferenceVote, Long> counts = new HashMap<EPreferenceVote, Long>();
+        for(EPreferenceVote vote : EPreferenceVote.values())
+            counts.put(vote, 0L);
+
+        List<TimeAdventurerPreference> prefs = all(placeOptionId);
+        for(TimeAdventurerPreference pref : prefs)
+            counts.put(pref.getVote(), counts.get(pref.getVote())+1);
+
+        return counts;
     }
 }
