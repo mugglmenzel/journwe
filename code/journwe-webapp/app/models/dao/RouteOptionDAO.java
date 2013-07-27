7fb6ce5f58dd6feb97c883d5b6e0ed3556745ae2
Markus <dontsendmeemails>
2013-07-28 00:16:05 +0200

    Added early Routes concept
    Routes are sequences of start-destination place plus time options.

--- code/journwe-webapp/app/models/dao/RouteOptionDAO.java
+++ code/journwe-webapp/app/models/dao/RouteOptionDAO.java
@@ -1,11 +1,37 @@
 package models.dao;
 
-/**
- * Created with IntelliJ IDEA.
- * User: markus
- * Date: 7/27/13
- * Time: 5:23 PM
- * To change this template use File | Settings | File Templates.
- */
-public class RouteOptionDAO {
+import models.adventure.place.PlaceOption;
+import models.adventure.route.RouteOption;
+import models.adventure.time.TimeOption;
+import models.dao.common.AdventureComponentDAO;
+
+public class RouteOptionDAO extends AdventureComponentDAO<RouteOption> {
+
+    public RouteOptionDAO() {
+        super(RouteOption.class);
+    }
+
+    public PlaceOption getStartPlace(String advId, String routeId) {
+        RouteOption route = get(advId,routeId);
+        if(route==null)
+            return null;
+        String placeId = route.getStartPlaceId();
+        return (new PlaceOptionDAO().get(advId,placeId));
+    }
+
+    public PlaceOption getEndPlace(String advId, String routeId) {
+        RouteOption route = get(advId,routeId);
+        if(route==null)
+            return null;
+        String placeId = route.getEndPlaceId();
+        return (new PlaceOptionDAO().get(advId,placeId));
+    }
+
+    public TimeOption getTime(String advId, String routeId) {
+        RouteOption route = get(advId,routeId);
+        if(route==null)
+            return null;
+        String timeId = route.getTimeId();
+        return (new TimeOptionDAO().get(advId,timeId));
+    }
 }
