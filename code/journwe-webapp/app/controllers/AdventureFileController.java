643c417250d286cb90ad6483ededa51e925d99a7
mugglmenzel <mugglmenzel@gmail.com>
2013-08-13 17:30:09 +0200

    files in adventure complete

--- code/journwe-webapp/app/controllers/AdventureFileController.java
+++ code/journwe-webapp/app/controllers/AdventureFileController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.Upload;
 import com.feth.play.module.pa.PlayAuthenticate;
@@ -27,7 +28,7 @@ import static play.data.Form.form;
 public class AdventureFileController extends Controller {
 
     public static final String S3_BUCKET = "journwe-files";
-    public static final String DEFAULT_STORAGE_PROVIDER="https://s3.amazonaws.com/"+S3_BUCKET;
+    public static final String DEFAULT_STORAGE_PROVIDER = "https://s3.amazonaws.com/" + S3_BUCKET;
 
     protected static Form<JournweFile> fileForm = form(JournweFile.class);
 
@@ -38,37 +39,39 @@ public class AdventureFileController extends Controller {
     @Security.Authenticated(SecuredBetaUser.class)
     public static Result uploadFile(String advId) {
         try {
-        if (!JournweAuthorization.canUploadFiles(advId))
-            return AuthorizationMessage.notAuthorizedResponse();
-        AuthUser usr = PlayAuthenticate.getUser(Http.Context.current());
-        if(usr==null)
-            throw new Exception("File upload failed because user is null.");
-        Form<JournweFile> filledFileForm = fileForm.bindFromRequest();
-        if (fileForm.hasErrors()) {
-            flash("error", "Sorry. Could not complete request because the form contains errors.");
-            return badRequest();
-        }
+            if (!JournweAuthorization.canUploadFiles(advId))
+                return AuthorizationMessage.notAuthorizedResponse();
+            AuthUser usr = PlayAuthenticate.getUser(Http.Context.current());
+            if (usr == null)
+                throw new Exception("File upload failed because user is null.");
+            Form<JournweFile> filledFileForm = fileForm.bindFromRequest();
+            if (fileForm.hasErrors()) {
+                flash("error", "Sorry. Could not complete request because the form contains errors.");
+                return badRequest();
+            }
 
-        Http.MultipartFormData body = request().body().asMultipartFormData();
-        Http.MultipartFormData.FilePart uploadFile = body.getFile("uploadFile");
-        File file = uploadFile.getFile();
+            Http.MultipartFormData body = request().body().asMultipartFormData();
+            Http.MultipartFormData.FilePart uploadFile = body.getFile("uploadFile");
+            if(uploadFile == null) return badRequest();
 
-        JournweFile journweFile = filledFileForm.get();
-        journweFile.setAdventureId(advId);
-        journweFile.setStorageProvider(DEFAULT_STORAGE_PROVIDER);
-        journweFile.setUserId(usr.getId());
-        String s3ObjectKey = advId+"/"+journweFile.getFileName();
-        String url = DEFAULT_STORAGE_PROVIDER+"/"+s3ObjectKey;
-        journweFile.setUrl(url);
-        if(!new JournweFileDAO().save(journweFile))
-            throw new Exception("Saving JournweFile in DynamoDB failed!");
-        // Upload files to S3 asynchronously
-        TransferManager tx = new TransferManager(credentials);
-        Upload upload = tx.upload(S3_BUCKET, s3ObjectKey, file);
-        flash("success", "Your files is uploading now and can be downloaded, soon...");
-        return ok(Json.toJson(journweFile));
+            File file = uploadFile.getFile();
+
+            JournweFile journweFile = filledFileForm.get();
+            journweFile.setAdventureId(advId);
+            journweFile.setStorageProvider(DEFAULT_STORAGE_PROVIDER);
+            journweFile.setUserId(usr.getId());
+            String s3ObjectKey = advId + "/" + journweFile.getFileName();
+            String url = DEFAULT_STORAGE_PROVIDER + "/" + s3ObjectKey;
+            journweFile.setUrl(url);
+            if (!new JournweFileDAO().save(journweFile))
+                throw new Exception("Saving JournweFile in DynamoDB failed!");
+            // Upload files to S3 asynchronously
+            TransferManager tx = new TransferManager(credentials);
+            Upload upload = tx.upload(S3_BUCKET, s3ObjectKey, file);
+            flash("success", "Your files is uploading now and can be downloaded, soon...");
+            return ok(Json.toJson(journweFile));
         } catch (Exception e) {
-            Logger.error(e.getMessage());
+            Logger.error("Failed uploading!", e);
             flash("error",
                     "File upload has failed. Sorry. Please try again later.");
             return internalServerError();
@@ -81,21 +84,24 @@ public class AdventureFileController extends Controller {
             return AuthorizationMessage.notAuthorizedResponse();
         Logger.debug("Listing files from DynamoDB ...");
         List<JournweFile> files = new JournweFileDAO().all(adventureId);
-        for(JournweFile file : files)
+        for (JournweFile file : files)
             Logger.debug(file.getFileName());
         return ok(Json.toJson(files));
     }
 
     @Security.Authenticated(SecuredBetaUser.class)
-    public static Result deleteFile(String id, String tid) {
-        if (!JournweAuthorization.canDeleteFiles(id))
+    public static Result deleteFile(String advId, String fileId) {
+        if (!JournweAuthorization.canDeleteFiles(advId))
             return AuthorizationMessage.notAuthorizedResponse();
+        JournweFile journweFile = new JournweFileDAO().get(advId, fileId);
 
-        new JournweFileDAO().delete(tid, id);
+        AmazonS3Client s3 = new AmazonS3Client(credentials);
+        s3.deleteObject(S3_BUCKET, advId + "/" + journweFile.getFileName());
 
-        // TODO delete file from S3
+        new JournweFileDAO().delete(journweFile);
 
-        return ok(); //TODO: Error handling
+
+        return ok();
     }
 
 }
