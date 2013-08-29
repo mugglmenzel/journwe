76d33ba9d7647be48cf9ed5f38d7a9fa31d757c6
Willi <w.tscheschner@gmail.com>
2013-08-29 21:10:04 +0200

    Ajaxify the notification popover (#81)
    Open issues:
    - Set read flag
    - TopicRef is 'null'

--- code/journwe-webapp/app/controllers/UserController.java
+++ code/journwe-webapp/app/controllers/UserController.java
@@ -2,13 +2,21 @@ package controllers;
 
 import models.auth.SecuredBetaUser;
 import models.dao.UserDAO;
+import models.dao.NotificationDAO;
 import models.notifications.ENotificationFrequency;
+import models.notifications.Notification;
 import models.user.User;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import views.html.user.get;
+import play.mvc.Http;
+import com.feth.play.module.pa.PlayAuthenticate;
+import java.util.List;
+import org.codehaus.jackson.node.ObjectNode;
+import org.codehaus.jackson.JsonNode;
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -34,5 +42,21 @@ public class UserController extends Controller {
             return ok(Json.toJson(usr.getNotificationDigest()));
         }
         return badRequest();
+    }
+
+
+    @Security.Authenticated(SecuredBetaUser.class)
+    public static Result getNotifications() {
+
+        User usr = new UserDAO().findByAuthUserIdentity(PlayAuthenticate.getUser(Http.Context.current()));
+        List<JsonNode> results = new ArrayList<JsonNode>();
+
+        for (Notification c : new NotificationDAO().all(usr.getId())) {
+            if (!c.isRead()){
+                results.add(Json.toJson(c));
+            }
+        }
+
+        return ok(Json.toJson(results));
     }
 }
