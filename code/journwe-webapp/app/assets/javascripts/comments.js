0178789bc3f2e1464057bf8ef87c16acbd7a9a2d
mugglmenzel <mugglmenzel@gmail.com>
2014-01-27 15:29:04 +0100

    comment js fix

--- code/journwe-webapp/app/assets/javascripts/comments.js
+++ code/journwe-webapp/app/assets/javascripts/comments.js
@@ -41,7 +41,7 @@ define([
         'click .btn-comment-add': function () {
             var val,
                 threadId = $(this).data('thread'),
-                input = $(this).closest('.input-comment').find('input:text');
+                input = $(this).closest('.comment-input').find('input:text');
 
             // Check if there is a value
             if (val = (input.val() || "").trim()) {
@@ -51,7 +51,7 @@ define([
         },
         'keyup .comment-input input:text': function (e) {
             if (e.keyCode == 13) {
-                $(this).closest('.input-comment').find('.btn-comment-add').click();
+                $(this).closest('.comment-input').find('.btn-comment-add').click();
             }
         }
     });
