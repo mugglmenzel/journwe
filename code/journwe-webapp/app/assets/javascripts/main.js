beb9702e98d83551308d0ee2f8b8dc47e07c46e3
mugglmenzel <mugglmenzel@gmail.com>
2014-02-02 17:27:06 +0100

    reenabled background in main.js

--- code/journwe-webapp/app/assets/javascripts/main.js
+++ code/journwe-webapp/app/assets/javascripts/main.js
@@ -3,7 +3,7 @@
  */
 
 require(['common/utils', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/2.0.0/js/bootstrap-switch.min.js'], function(utils){
-    /*
+
     var lastScrollTop = 0,
         bgr = $('#background'),
         adv = $('#adventure-container');
@@ -15,6 +15,7 @@ require(['common/utils', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switc
         bgr.css('background-image', 'url("http://i.embed.ly/1/image/resize?width=1600&key=2c8ef5b200c6468f9f863bc75c46009f&url=http%3A%2F%2Fwww.journwe.com%2Fassets%2Fimg%2Fbg%2F' + images[Math.floor(Math.random() * images.length)] + '")');
     }
 
+    /*
     // Set parallex scrolling
     var top = 130;
     $(window).scroll($.throttle(0, function(event){
