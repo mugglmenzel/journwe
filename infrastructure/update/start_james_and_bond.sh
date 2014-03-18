35afbd83247f956772dc22fa3b630225cb0e4efe
mugglmenzel <mugglmenzel@gmail.com>
2014-03-18 13:58:54 +0100

    restructured and updated deployment scripts

--- infrastructure/update/start_james_and_bond.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-# kill all screens
-screen -ls | grep pts | cut -d. -f1 | awk '{print $1}' | xargs kill
-
-# launch screens
-
-sudo screen -S bond -dm -t bond bash -c 'sudo java -jar /home/ubuntu/james/conf/lib/journwe-james-0.1-SNAPSHOT-jar-with-dependencies.jar'
-sudo screen -S bond -dm -t bond bash -c '(cd /home/ubuntu/james/bin/; sudo ./run.sh)'
-
--- infrastructure/update/update_james_from_local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-(cd ../../code/journwe-james; mvn clean compile assembly:single)
-scp -i journwe.pem ../../code/journwe-james/target//journwe-james-*-with-dependencies.jar ubuntu@54.221.210.118:/home/ubuntu/james/conf/lib/.
