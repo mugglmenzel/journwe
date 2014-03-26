68a607bf12b08f086a7e990eebdfa4c9fc276703
markusklems <markusklems@gmail.com>
2014-03-26 21:12:43 +0100

    Added visible s3cfg file.
    The invisible .s3cfg was ignored by git.

--- infrastructure/scripts/push_assets_to_s3.sh
+++ infrastructure/scripts/push_assets_to_s3.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
 S3CMD="../s3cmd/s3cmd"
-CONF="--config=../s3cmd/.s3cfg"
-ASSETDIR="../../code/journwe-webapp/target/scala-2.10/classes/public"
+CONF="--config=../s3cmd/s3cfg"
+ASSETDIR1="../../code/journwe-webapp/target/scala-2.10/classes/public/"
+ASSETDIR2="../../code/journwe-webapp/modules/acl/target/scala-2.10/classes/public/"
+ASSETDIR3="../../code/journwe-webapp/modules/admin/target/scala-2.10/classes/public/"
+ASSETDIR4="../../code/journwe-webapp/modules/core/target/scala-2.10/classes/public/"
+ASSETDIR5="../../code/journwe-webapp/modules/data/target/scala-2.10/classes/public/"
+ASSETDIR6="../../code/journwe-webapp/modules/dataCache/target/scala-2.10/classes/public/"
+ASSETDIR7="../../code/journwe-webapp/modules/email/target/scala-2.10/classes/public/"
+ASSETDIR8="../../code/journwe-webapp/modules/social/target/scala-2.10/classes/public/"
 S3BUCKET="s3://journwe-assets/"
 
-echo "uploading local assets to s3 bucket now..."
-$S3CMD $CONF sync --delete-removed --recursive $ASSETDIR $S3BUCKET
+echo "check md5 hash and uploade changed local assets to s3 bucket now... sorry, this can take a while ..."
+echo "checking and uploading $ASSETDIR1"
+$S3CMD $CONF sync --recursive $ASSETDIR1 $S3BUCKET
+echo "checking $ASSETDIR2"
+$S3CMD $CONF sync --recursive $ASSETDIR2 $S3BUCKET
+echo "checking $ASSETDIR3"
+$S3CMD $CONF sync --recursive $ASSETDIR3 $S3BUCKET
+echo "checking $ASSETDIR4"
+$S3CMD $CONF sync --recursive $ASSETDIR4 $S3BUCKET
+echo "checking $ASSETDIR5"
+$S3CMD $CONF sync --recursive $ASSETDIR5 $S3BUCKET
+echo "checking $ASSETDIR6"
+$S3CMD $CONF sync --recursive $ASSETDIR6 $S3BUCKET
+echo "checking $ASSETDIR7"
+$S3CMD $CONF sync --recursive $ASSETDIR7 $S3BUCKET
+echo "checking $ASSETDIR8"
+$S3CMD $CONF sync --recursive $ASSETDIR8 $S3BUCKET
 echo "make assets accessible to everyone"
 $S3CMD $CONF setacl $S3BUCKET --acl-public --recursive
