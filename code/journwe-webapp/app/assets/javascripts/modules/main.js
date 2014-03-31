a42b7750ef516fc757b096b80f7134d87a417924
mugglmenzel <mugglmenzel@gmail.com>
2014-03-31 13:40:09 +0200

    improved javascript loading and structure, improved landing page, streamlined indexVet page

--- code/journwe-webapp/app/assets/javascripts/modules/main.js
+++ code/journwe-webapp/app/assets/javascripts/modules/main.js
@@ -5,7 +5,7 @@ define([
 ], function (utils, header, journwe) {
     journwe(function () {
         utils.on({
-            'click btn-scroll-top': function () {
+            'click .btn-scroll-top': function () {
                 $('html, body').animate({ scrollTop: 0 }, 600);
                 return false;
             }
